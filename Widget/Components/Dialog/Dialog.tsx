281ae940c0ccc82489ea0132c1414ec420c95b19
Roman Ohiienko <ohiienko.r@gmail.com>
2024-09-21 19:58:28 +0300

    feat: add swipe down gesture for closing modal

--- Widget/Components/Dialog/Dialog.tsx
+++ Widget/Components/Dialog/Dialog.tsx
@@ -1,6 +1,7 @@
 import { Modal, View } from "react-native";
 import { SafeAreaView } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import GestureRecognizer from "react-native-swipe-gestures";
 import CloseButton from "../CloseButton/CloseButton";
 import { DialogPropTypes } from "./types";
 import { styles } from "./Dialog.styles";
@@ -9,22 +10,24 @@ const Dialog = ({ open, onClose, children }: DialogPropTypes) => {
   const insets = useSafeAreaInsets();
 
   return (
-    <Modal animationType="slide" visible={open} onRequestClose={onClose}>
-      <SafeAreaView style={{ flex: 1 }}>
-        <View
-          style={[
-            styles.dialog,
-            {
-              paddingTop: insets.top,
-              paddingBottom: insets.bottom,
-            },
-          ]}
-        >
-          <CloseButton onPress={onClose} />
-          {children}
-        </View>
-      </SafeAreaView>
-    </Modal>
+    <GestureRecognizer style={{ flex: 1 }} onSwipeDown={onClose}>
+      <Modal animationType="slide" visible={open} onRequestClose={onClose}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <View
+            style={[
+              styles.dialog,
+              {
+                paddingTop: insets.top,
+                paddingBottom: insets.bottom,
+              },
+            ]}
+          >
+            <CloseButton onPress={onClose} />
+            {children}
+          </View>
+        </SafeAreaView>
+      </Modal>
+    </GestureRecognizer>
   );
 };
 
